ee6093ed21d1a000df790dd9476fbc5cfe760605
Brenton Reeder <brenton.reeder@betabytesoftware.com>
2016-12-05 18:07:22 -0500

    DateBefore Toggle
    Creating a before-date toggle for #6

---
+++ FeatureFlags/Toggles/DateBeforeToggle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FeatureFlags.Providers;
+
+namespace FeatureFlags.Toggles
+{
+    public abstract class DateBeforeToggle : FeatureToggle, IFeatureToggle
+    {
+        protected  DateBeforeToggle() : this(new AppSettingsProvider()) { }
+        protected DateBeforeToggle(IFeatureProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                var now = DateTime.Now;
+
+                var value = base.GetKey();
+
+                DateTime timeToCompare;
+                if (DateTime.TryParse(value, out timeToCompare))
+                {
+                    return now < timeToCompare;
+                }
+                return false;
+            }
+        }
+    }
+}
