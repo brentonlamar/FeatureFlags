c3b8c076a76ad8aff9e46cd0c8bb2748962a46a1
Jamie <tidusjar@gmail.com>
2016-12-08 13:10:55 +0000

    Added After DateTime feature toggle with Unit tests #5

---
+++ FeatureFlags/Toggles/DateAfterFeatureToggle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FeatureFlags.Providers;
+
+namespace FeatureFlags.Toggles
+{
+    public abstract class DateAfterFeatureToggle : FeatureToggle, IFeatureToggle
+    {
+        protected DateAfterFeatureToggle() : this(new AppSettingsProvider()) { }
+        protected DateAfterFeatureToggle(IFeatureProvider provider)
+        {
+            _provider = provider;
+        }
+        public bool Enabled
+        {
+            get
+            {
+                var now = DateTime.Now;
+
+                var value = GetKey();
+
+                DateTime parsedDate;
+                if (!DateTime.TryParse(value, out parsedDate))
+                {
+                    throw new ArgumentException($"Invalid date: {value}");
+                }
+
+                return parsedDate > now;
+            }
+        }
+    }
+}
